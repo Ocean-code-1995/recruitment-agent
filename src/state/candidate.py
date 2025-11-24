6e362ea1fbf76190111db103202ff2475fbe47f9
sebastian wefers <sebastianwefersnz@gmail.com>
2025-11-24 22:53:20 +0100

    more fine grained levels

--- src/state/candidate.py
+++ src/state/candidate.py
@@ -2,8 +2,10 @@ import enum
 
 class CandidateStatus(str, enum.Enum):
     applied = "applied"
-    cv_screened = "cv_screened"
-    voice_done = "voice_done"
+    cv_passed = "cv_passed"
+    cv_rejected = "cv_rejected"
+    voice_passed = "voice_passed"
+    voice_rejected = "voice_rejected"
     interview_scheduled = "interview_scheduled"
     decision_made = "decision_made"
 
